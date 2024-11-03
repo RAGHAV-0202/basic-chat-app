3911da7985f6cb3af30d8c1ca7a71d37b9837c01
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-11-03 20:11:18 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./index.css"
 import React from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8000')
+const socket = io('https://basic-chat-app-8brn.onrender.com/')
 
 function JoinRoom({setStage , setRoomId , setName}){
 
